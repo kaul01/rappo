eeed3474059970fafb20d8a8286b44a18fac68c0
saiesh <saieshkaul01@gmail.com>
2021-06-21 08:36:51 +0530

    21st June;

--- mail.php
+++ mail.php
@@ -3,7 +3,7 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         # FIX: Replace this email with recipient email
-        $mail_to = "sawanjoshi1996@gmail.com";
+        $mail_to = "saieshkaul_2k18co311@dtu.ac.in";
         
         # Sender Data
         $subject = trim($_POST["subject"]);
